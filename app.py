06dd910278cef8c5a555c788f65d63fa4d7108ff
黃承翰 <131173461+je995301@users.noreply.github.com>
2023-12-06 00:37:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -193,12 +193,12 @@ def handle_message(event):
 
 def format_menu(health_menu):
     # 菜單格式化
-    if health_menu is not None:
+    if health_menu:
         menu_items = health_menu.split('\n')
         formatted_menu = "健康菜單：\n"
         for item in menu_items:
-            if item is not None and item.strip():
-                formatted_menu += f"- {item.strip()}\n" if item is not None and item.strip() else ""
+            if item and item.strip():
+                formatted_menu += f"- {item.strip()}\n"
         return formatted_menu
     else:
         return "無法生成健康菜單。"
