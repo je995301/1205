cf068da580fb799c77cc52ad9caa58b06d767ab6
黃承翰 <131173461+je995301@users.noreply.github.com>
2023-12-06 00:17:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ def format_menu(health_menu):
         formatted_menu = "健康菜單：\n"
         for item in menu_items:
             if item is not None and item.strip():
-                formatted_menu += f"- {item.strip()}\n"
+                formatted_menu += f"- {item.strip()}\n" if item else ""
         return formatted_menu
     else:
         return "無法生成健康菜單。"
